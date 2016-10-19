b150d5d711e964e7ab2dc63ee24ac461a7a6948b
Ali Camargo <ali.camargo@hotmail.com>
2016-10-18 19:57:07 -0500

    Add connection refused error

--- app/js/config/app.config.js
+++ app/js/config/app.config.js
@@ -5,8 +5,20 @@ angular
     .module("app")
     .config(appConfig);
 
-appConfig.$inject =['RestangularProvider', 'ENDPOINT_URL'];
+appConfig.$inject =['$httpProvider', 'RestangularProvider', 'ENDPOINT_URL'];
 
-function appConfig(RestangularProvider, ENDPOINT_URL) {
+function appConfig($httpProvider, RestangularProvider, ENDPOINT_URL) {
     RestangularProvider.setBaseUrl(ENDPOINT_URL);
+
+    $httpProvider.interceptors.push(function($q, toastr) {
+        return {
+            responseError: function(rejection) {
+                if(rejection.status <= 0) {
+                    toastr.error('Connection refused', 'Error');
+                    return false;
+                }
+                return $q.reject(rejection);
+            }
+        };
+    });
 }
